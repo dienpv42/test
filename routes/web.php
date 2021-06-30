b33343d1a3fcf0e880764b5cc076f79a8a947c90
dienpv <dienphamvan@vccorp.vn>
2021-06-30 17:57:53 +0700

    f

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('api')->group(function(){
+    // get contact
+    Route::get('getContacts', 'App\Http\Controllers\ContactController@getContact');
+});
